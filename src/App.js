db91a8b0d1da2a2d207e97102123fff8b0595df5
PMSRINIVAS <killermaddy55@gmail.com>
2021-06-30 21:51:27 +0530

    Accessing the variables in jsx.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 //Special in the world of react family
-function App() {
-  //Function that returns JSX/ HTML TAG
-  return <h1> Hello World React JSXXXX!!HI,,,,</h1>
-}
-
-const sum=(n1,n2) =>{
-  return n1+n2;
+const App = () => {
+  //local variable
+  let counter=100;
+  let username="Capgemini";
+  return (
+  <div> 
+    <h1>Hello World123</h1>
+    <h1>{username}</h1>
+    <h1>{counter}</h1>
+  </div>
+  );
 };
 
 export default App;
