124a158fdb394769966ddb682e76ab66da807fb6
PMSRINIVAS <killermaddy55@gmail.com>
2021-06-30 22:19:37 +0530

    events in react.

--- src/App.js
+++ src/App.js
@@ -5,27 +5,19 @@ import './App.css';
 const App = () => {
   //local variable
   let counter=100;
-  let username="Capgemini";
-  let list=["Delhi", "Kolkata","Mumbai","Chennai"];
+  
+  const increment = ()=> {
+   // alert("I am an increment function");
+    counter +=1;
+    console.log("I AM INCREMENT FUNCTION", counter);
+  };
+
   return (
   <div> 
-    <h1>Hello World123</h1>
-    <h1>{username}</h1>
-    <h1>{counter}</h1>
-
-//By using .map we can get the list items dynamically
-    <h1>Dynamic Web Site</h1>
-    {list.map((item) => (
-      <h3>{item}</h3>
-    )
-    )}
-   //hardcode it manually 
-    <h1>Static Web Site</h1>
-    <h3>Delhi</h3>
-    <h3>Kolkata</h3>
-    <h3>Mumbai</h3>
-    <h3>Chennai</h3>
-  </div>
+    <h1>Event Demo</h1>
+    <h3>{counter}</h3>
+    <input type="button" value="Increment" onClick={increment} />
+  </div>  
   );
 };
 
